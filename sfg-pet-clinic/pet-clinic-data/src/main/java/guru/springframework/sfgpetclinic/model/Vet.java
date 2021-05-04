f4f664671ac47aec0c7a410acd5eff6bfa7f95df
JerryDenzinger <gerardo_denzinger@outlook.de>
2021-05-04 19:27:47 +0200

    Add Lombok Functionality

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -9,20 +9,23 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
-	
+
 	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(name = "vet_specialtties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name= "specialty_id"))
+	@JoinTable(name = "vet_specialtties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "specialty_id"))
 	private Set<Speciality> specialities = new HashSet<>();
-
-	public Set<Speciality> getSpecialities() {
-		return specialities;
-	}
-
-	public void setSpecialities(Set<Speciality> specialities) {
-		this.specialities = specialities;
-	}
-
 }
