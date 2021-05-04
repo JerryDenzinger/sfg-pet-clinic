c81ae342136730dff680bdaa3474eae5e13a1bf6
JerryDenzinger <gerardo_denzinger@outlook.de>
2021-05-04 16:14:00 +0200

    Implemented MapImpl for Visit Service. Closes #46

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -16,7 +18,8 @@ public class Visit extends BaseEntity {
 	@Column(name = "description")
 	private String description;
 	
-	
+	@ManyToOne
+	@JoinColumn(name = "pet_id")
 	private Pet pet;
 	
 	
