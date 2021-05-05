c0c01e598e8e35de802969249336a670bafba790
JerryDenzinger <gerardo_denzinger@outlook.de>
2021-05-05 18:20:30 +0200

    Mockito Extensions Junit test

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
@@ -0,0 +1,56 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import guru.springframework.sfgpetclinic.repositories.OwnerRepository;
+import guru.springframework.sfgpetclinic.repositories.PetRepository;
+import guru.springframework.sfgpetclinic.repositories.PetTypeRepository;
+
+class OwnerSDJpaServiceTest {
+	OwnerRepository ownerRepository;
+	PetRepository petRepository;
+	PetTypeRepository petTypeRepository;
+	
+	OwnerSDJpaService ownerSDJpaService;
+	
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	void sindByLastNameTest() {
+
+	}
+	
+	@Test
+	void FindAllTest() {
+
+	}
+	
+	@Test
+	void findByIdTest() {
+
+	}
+	
+	@Test
+	void saveTest() {
+
+	}
+	
+	@Test
+	void deleteTest() {
+
+	}
+	
+	@Test
+	void deleteByIdTest() {
+
+	}
+
+
+}
