b33b18ba3deb59deed4d99136232c8c0ad5767a4
JerryDenzinger <gerardo_denzinger@outlook.de>
2021-05-06 11:11:19 +0200

    Fixed problem with mockito dependency

--- sfg-pet-clinic/pet-clinic-data/src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
+++ sfg-pet-clinic/pet-clinic-data/src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
@@ -2,55 +2,66 @@ package guru.springframework.sfgpetclinic.services.springdatajpa;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import guru.springframework.sfgpetclinic.repositories.OwnerRepository;
 import guru.springframework.sfgpetclinic.repositories.PetRepository;
 import guru.springframework.sfgpetclinic.repositories.PetTypeRepository;
 
+@ExtendWith(MockitoExtension.class)
 class OwnerSDJpaServiceTest {
+	
+	@Mock
 	OwnerRepository ownerRepository;
+	@Mock
 	PetRepository petRepository;
+	@Mock
 	PetTypeRepository petTypeRepository;
 	
-	OwnerSDJpaService ownerSDJpaService;
-	
+	OwnerSDJpaService service;
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
+	@BeforeEach
+	void setUp() throws Exception {
 	}
 
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
 	@Test
 	void sindByLastNameTest() {
 
 	}
-	
+
 	@Test
 	void FindAllTest() {
 
 	}
-	
+
 	@Test
 	void findByIdTest() {
 
 	}
-	
+
 	@Test
 	void saveTest() {
 
 	}
-	
+
 	@Test
 	void deleteTest() {
 
 	}
-	
+
 	@Test
 	void deleteByIdTest() {
 
 	}
-
 
 }
