0b32b2ea5c8a5412459c4803e205bcd424a4ddff
JerryDenzinger <gerardo_denzinger@outlook.de>
2021-05-14 10:24:50 +0200

    Revert "Owner controller tests display"
    This reverts commit 8fa4363a8871ebf014a1be9450e1df57702cf274.

--- sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
@@ -2,10 +2,7 @@ package guru.springframework.sfgpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import guru.springframework.sfgpetclinic.services.OwnerService;
 
@@ -28,12 +25,5 @@ public class OwnerController {
 	@RequestMapping({ "/find" })
 	public String findOwners() {
 		return "notimplemented";
-	}
-	
-	@GetMapping("/{ownerId}")
-	public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
-		ModelAndView mav = new ModelAndView("owners/ownerDetails");
-		mav.addObject(ownerService.findById(ownerId));
-		return mav;
 	}
 }
